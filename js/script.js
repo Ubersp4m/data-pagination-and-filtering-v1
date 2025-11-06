89ffb22f131a90674d0de27289d8660b037fa4ea
Ubersp4m <109011348+Ubersp4m@users.noreply.github.com>
2025-11-05 17:17:22 -0800

    completed project

--- js/script.js
+++ js/script.js
@@ -12,26 +12,132 @@ For assistance:
 */
 
 
+const numPages = 9;
+const buttonsUL = document.querySelector('.link-list');
+const search = document.querySelector('#search');
+const searchButton = document.querySelector('.student-search > button');
 
 /*
-Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage(array, page){
+function showPage(list, page){
+   const startIndex = (page * numPages) - numPages;
+   const endIndex = page * numPages;
+   const studentListUL = document.querySelector('.student-list');
+   studentListUL.innerHTML = '';
 
-
-}
-
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-function addPagination(){
-
+   for(student in list){
+           if(student>=startIndex && student<endIndex){ 
+         let html = `
+            <li class="student-item cf">
+                <div class="student-details">
+                  <img class="avatar" src="${list[student].picture.large}" alt="Profile Picture">
+                  <h3>${list[student].name.first} ${list[student].name.last}</h3>
+                  <span class="email">${list[student].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${list[student].registered.date}</span>
+               </div>
+            </li>
+         `;
+         studentListUL.insertAdjacentHTML("beforeend", html);
+      }
+   }
    
 }
 
 
+/*
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+function addPagination(list){
+   let numButtons = Math.ceil(list.length / numPages);
+   let buttonActive = document.querySelector('.active'); 
+   buttonsUL.innerHTML = '';
 
+   for(let i = 1; i<=numButtons; i++){
+      let html = `
+         <li>
+            <button type='button'>${i}</button>
+         </li>
+      `;
+      buttonsUL.insertAdjacentHTML('beforeend', html);
+   }
+
+   //keeps track of active class on buttons after clearing innerHTML of the pagination ul
+   if(!buttonActive){
+      let button = document.querySelector('.link-list li > :first-child');
+      button.classList.add('active');   
+      }
+   else { //selects new button from innerHTML and sets active class
+      buttonActive=document.querySelector(`.link-list li:nth-child(${buttonActive.textContent}) button`);
+      buttonActive.classList.add('active');
+   }
+   
+ 
+}
+
+  /*
+      gets input from search input
+      adds students to searchResults[] array
+      calls addPagination and showPage with search results array
+   */
+function searchStudents(students, page){
+   let searchResults = [];
+
+   for(let student of students){
+      let name = student.name.first.toLowerCase() + ' ' +student.name.last.toLowerCase();
+      let searchName = search.value.toLowerCase();
+      
+      if(name.includes(searchName)){
+         searchResults.push(student);
+      }
+   }
+   if(searchResults.length === 0){
+      let ul = document.querySelector('.student-list');
+      ul.innerHTML='';
+      buttonsUL.innerHTML = '';
+      html=`<h3>no students found</h3>`;
+      ul.insertAdjacentHTML('beforeend',html);
+   }
+   else{
+      addPagination(searchResults);
+      showPage(searchResults, page);
+   }
+   
+}
+
+/*
+   event listener to set active pagination class on button clicked
+*/
+buttonsUL.addEventListener('click', (e) =>{
+      let button1 = document.querySelector('.active');
+      
+      if(e.target.type === 'button'){
+         let button2 = e.target.closest('button');
+         button1.classList.remove('active');
+         button2.classList.add('active'); 
+         if(search.value===''){
+            showPage(data, button2.textContent);
+            addPagination(data);
+         }
+         else{
+            searchStudents(data, button2.textContent);
+         }
+        
+         
+      }
+
+}); 
+
+search.addEventListener('keyup', () => {
+   searchStudents(data, 1);
+});
+
+searchButton.addEventListener('click', () => {
+   searchStudents(data, 1);
+});
+
+showPage(data, 1);
+addPagination(data, false);
 // Call functions
